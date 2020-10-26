b07741a5394bf35152c3a0f5f0306eab7cfec8b6
Ben Calvert <benhcalvert@gmail.com>
2020-10-26 09:42:48 -0600

    updated routes with static html page from Cloudflare

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Router = require('./router');
 import { Store } from "./store";
 import { Html } from "./html";
+// import css from 'style.css';
+
+const page = 'https://static-links-page.signalnerve.workers.dev';
 
 /**
  * Example of how router can be used in an application
@@ -21,20 +24,20 @@ async function handleRequest(request) {
     try{
         const r = new Router()
         // Replace with the appropriate paths and handlers
-        r.get('.*/bar', () => new Response('responding for /bar'))
+        // r.get('.*/bar', () => new Response('responding for /bar'))
         r.get('.*/links', request => handler(request))
-        r.post('.*/foo.*', request => handler(request))
-        r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
+        // r.post('.*/foo.*', request => handler(request))
+        r.get('/', request => fetch(page)) // return the response from the origin
         
         // return a default message for the root route
         // r.get('/', () => new Response("Welcome to Ben Calvert's App")) 
     
         // should return html from html.js module
-        r.get('/', () => new Response(Html, {
-            headers: {
-              "content-type": "text/html;charset=UTF-8",
-            },
-          }))    
+        // r.get('/', () => new Response(Html, {
+        //     headers: {
+        //       "content-type": "text/html;charset=UTF-8",
+        //     },
+        //   }))    
     
         const resp = await r.route(request)
         return resp
